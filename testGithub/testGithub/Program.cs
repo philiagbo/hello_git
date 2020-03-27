279b2f66599610abb6c1969a21098dfda04bf303
philiagbo <31472945+philiagbo@users.noreply.github.com>
2020-03-27 14:57:51 +0000

    hello
    hello

---
+++ testGithub/testGithub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello Github");
+            Console.WriteLine("next line");
+            Console.ReadLine();
+        }
+    }
+}
